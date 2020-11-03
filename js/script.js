cc7348b4492aaf7bc2f69850bbb11637da440f04
Cristian Gardosi <cristian.gardosi97@gmail.com>
2020-11-03 17:12:10 +0100

    3/3

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
 
     // JSnack 1 ******************************************
+    // FOREACH *******************************************
     // Creare un array di oggetti
     // Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
     // Stampare a schermo la bici con peso minore con destructuring e template literal.
@@ -29,7 +30,9 @@ $(document).ready(function () {
     );
 
 
+
     // JSnack 2 ******************************************
+    // FILTER ********************************************
     // Partendo da un array di nomi (scelti da voi) creiamo un nuovo array.
     // Chiedere due numeri all'utente per definire la posizione iniziale e finale dal quale estrarre i valori dall'array originale.
     // Il nuovo array deve contenere gli elementi dell'array originale aventi la posizione (indice array)  compresa tra i due numeri inseriti dall'utente.
@@ -46,12 +49,58 @@ $(document).ready(function () {
 
     // Caso di filter in cui voglio estrapolare una parte dell'array esistente e filtrare alcuni elementi in un array nuovo
     rangedNames = names.filter((element, index) => {
-            // Returno il mio nuovo array se rispetta la condizione similarmente a come lavora, con una sintassi diversa, if.
+            // Returno il mio nuovo array se rispetta la condizione. Filter valuta la sua condizione con questa sintassi che risulta molto più breve e semplificata di quella vista con gli if interni ai for/while e specifica, per l'appunto, di filter.
             return min <= index && max >= index;
     });
     console.log(rangedNames);
 
 
+
+
+    // JSnack 3 ******************************************
+    // SPREAD e MAP **************************************
+    // Creiamo un array di oggetti (scelti da voi)
+    // Creiamo una copia da questo array di partenza e aggiungiamo ai singoli elementi una nuova proprietà position che contiene una lettera casuale.
+    // Non dobbiamo modificare l'array iniziale.
+
+    var arrayOggetti = [
+        {
+            nome: 'RayBan',
+            tipo: 'Occhiali da sole',
+            prezzo: 178
+        },
+        {
+            nome: 'Carrera',
+            tipo: 'Occhiali da sole',
+            prezzo: 139
+        },
+        {
+            nome: 'Persol',
+            tipo: 'Occhiali da vista',
+            prezzo: 399
+        }
+    ];
+
+    const newArray = [...arrayOggetti].map(occhiali => {
+        return { 
+            ...occhiali,
+            position: generaLettere()
+        } 
+    });
+
+
+
+// FUNZIONE GENERA LETTERE RANDOM
+function generaLettere() {
+    var lettere = 'abcdefghilmnopqrstuvz';
+    var letteraGenerata = lettere[numeriRandom(0, lettere.length - 1)];
+    return letteraGenerata;
+
+}
+// Numeri random
+function numeriRandom(min, max) {
+    return Math.floor( Math.random() * (max - min + 1) ) + min;
+}
     
 
 
